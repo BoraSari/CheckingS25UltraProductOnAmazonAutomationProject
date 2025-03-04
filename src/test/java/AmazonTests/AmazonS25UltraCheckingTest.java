90c3a543359943ac54c88245df1f38a451644933
BoraSari <borasari04@gmail.com>
2025-03-03 23:14:19 -0500

    Test Results shared on Github.

--- src/test/java/AmazonTests/AmazonS25UltraCheckingTest.java
+++ src/test/java/AmazonTests/AmazonS25UltraCheckingTest.java
@@ -3,6 +3,7 @@ package AmazonTests;
 import AmazonPages.AmazonMainPage;
 import AmazonPages.S25UltraDetailPage;
 import AmazonPages.SmartPhonesResultPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AmazonS25UltraCheckingTest extends BaseTest{
@@ -22,6 +23,9 @@ public class AmazonS25UltraCheckingTest extends BaseTest{
         detailPage= new S25UltraDetailPage(getDriver());
         softAssert.assertTrue(detailPage.displayProductNotFoundMessage().isDisplayed());
         softAssert.assertEquals("Sorry! We couldn't find that page. Try searching or go to Amazon's home page.",detailPage.getProductNotFoundErrorMessage());
+        Assert.fail("Expected Result: Amazon Web App should display 'S25 Ultra' on the user's screen.");
+        Assert.fail("Actual Result: The application cannot display 'S25 Ultra' on the user's screen. The app throws the message: 'Sorry! We couldn't find that page. Try searching or go to Amazon's home page.'");
+
     }
 
 }
